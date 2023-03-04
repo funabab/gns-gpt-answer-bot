3ef9cb09f5e0ef1ee86ddec0453a49e59610e0c0
Abdulbassit Abdullahi <funabab@gmail.com>
2023-03-04 22:49:35 +0100

    feat: serving frontend from nest

--- frontend-client/tailwind.config.cjs
+++ frontend-client/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', 'src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: 'Söhne,ui-sans-serif,system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;',
+      },
+    },
   },
   plugins: [],
-};
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
+import { join } from 'node:path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend-client', 'dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
